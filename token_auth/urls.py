08c623132c56e1db372a2da5d3ed630bff6db522
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2020-05-23 23:34:21 +0700

    Add trailing slashes in urls

--- token_auth/urls.py
+++ token_auth/urls.py
@@ -4,11 +4,11 @@ from django.views.decorators.csrf import csrf_exempt
 from token_auth import views
 
 urlpatterns = [
-    path('auth', csrf_exempt(views.LoginView.as_view())),
-    path('users', csrf_exempt(views.SignUpView.as_view())),
+    path('auth/', csrf_exempt(views.LoginView.as_view())),
+    path('users/', csrf_exempt(views.SignUpView.as_view())),
 
     path('profile/<int:pk>', csrf_exempt(views.ProfileView.as_view())),
     path('profile/me/', csrf_exempt(views.MyProfileView.as_view())),
     path('profile/upload', csrf_exempt(views.UploadPhotoView.as_view())),
-    path('tokens', csrf_exempt(views.FirebaseTokenView.as_view())),
+    path('tokens/', csrf_exempt(views.FirebaseTokenView.as_view())),
 ]
