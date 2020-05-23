f390eade9aa40e73fb53b79c306882a8bd8100d5
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2020-05-23 20:01:49 +0700

    Setup logging

--- MeetingApp/settings.py
+++ MeetingApp/settings.py
@@ -195,3 +195,38 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'filters': [],
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': 'console',
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+    }
+}
