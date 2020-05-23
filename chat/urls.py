ece4db36263894534e46ee1f733b736e91e1b675
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2020-05-23 22:18:20 +0700

    Trailing slash is a must in urls, otherwise we get 404

--- chat/urls.py
+++ chat/urls.py
@@ -4,11 +4,11 @@ from django.views.decorators.csrf import csrf_exempt
 from .views import *
 
 urlpatterns = [
-    path('chats', csrf_exempt(ChatsView.as_view())),
+    path('chats/', csrf_exempt(ChatsView.as_view())),
 
     path('messages/<int:event_id>', csrf_exempt(MessageView.as_view())),
-    path('messages', csrf_exempt(MessageView.as_view())),
+    path('messages/', csrf_exempt(MessageView.as_view())),
 
     path('private-messages/<int:user_id>', csrf_exempt(PrivateMessageView.as_view())),
-    path('private-messages', csrf_exempt(PrivateMessageView.as_view())),
+    path('private-messages/', csrf_exempt(PrivateMessageView.as_view())),
 ]
